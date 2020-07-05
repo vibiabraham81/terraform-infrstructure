6ef73102be10aa3e34fd0703a861de0e3d65125a
vibiabraham81 <66189131+vibiabraham81@users.noreply.github.com>
2020-07-05 16:48:27 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,3 +12,11 @@ resource "azurerm_resource_group" "rg" {
         Team = "DevOps"
     }
 }
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+    name                = "myTFVnet"
+    address_space       = ["10.0.0.0/16"]
+    location            = "westus2"
+    resource_group_name = azurerm_resource_group.rg.name
+}
